974d0d31af9fe20e7a83e74e1b4046a1ee46bf1c
TritonEnigmatico <stepan.karoly@gmail.com>
2026-01-26 12:23:46 +0100

    konstruktor třídy Zvíře

--- Utulek/Zvire.cs
+++ Utulek/Zvire.cs
@@ -8,6 +8,21 @@ namespace Utulek
 {
     internal class Zvire
     {
-        Console.WriteLine("ahoj");
+        public string Jmeno {get;set;}
+        public string Druh {get;set;}
+        public string Pohlavi {get;set;}
+        public bool Adoptovano {get;set;}
+        public int Vek {get;set;}
+        public static int ID {get;set;}
+
+        public Zvire(string jmeno, string druh, string pohlavi, bool adoptovano, int vek)
+    {
+        Jmeno = jmeno;
+        Druh = druh;
+        Pohlavi = pohlavi;
+        Adoptovano = adoptovano;
+        Vek = vek;
+        ID++;
+    }
     }
 }
