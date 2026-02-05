6a37026644339ad69b99ffeb328f640e75110273
TritonEnigmatico <stepan.karoly@gmail.com>
2026-02-05 08:51:40 +0100

    Oprava v indexování

--- Utulek/Zvire.cs
+++ Utulek/Zvire.cs
@@ -8,6 +8,7 @@ namespace Utulek
 {
     internal class Zvire
     {
+        private static int _pocitadloId = 0;
         public string Jmeno {get;set;}
         public string Druh {get;set;}
         public string Pohlavi {get;set;}
@@ -20,12 +21,13 @@ namespace Utulek
 
         public Zvire(string jmeno, string druh, string pohlavi, bool adoptovano, int vek, DateTime datumPrijmu, string zdravotniStav, string poznamka = null)
     {
+        _pocitadloId++;
         Jmeno = jmeno;
         Druh = druh;
         Pohlavi = pohlavi;
         Adoptovano = adoptovano;
         Vek = vek;
-        ID++;
+        ID = _pocitadloId;
         DatumPrijmu = datumPrijmu;
         ZdravotniStav = zdravotniStav;
         Poznamka = poznamka;
